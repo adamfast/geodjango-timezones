43fa553923cf1528ba0ad3ce6ecf26efb2e24e96
Adam Fast <adam@adamfast.com>
2015-01-19 19:22:51 -0600

    South migrations get moved and replaced with Django 1.7 migrations.

--- timezones/migrations/0001_initial.py
+++ timezones/migrations/0001_initial.py
@@ -1,35 +1,25 @@
-# encoding: utf-8
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-        
-        # Adding model 'Timezone'
-        db.create_table('timezones_timezone', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('tzid', self.gf('django.db.models.fields.CharField')(max_length=128)),
-            ('mpoly', self.gf('django.contrib.gis.db.models.fields.MultiPolygonField')()),
-        ))
-        db.send_create_signal('timezones', ['Timezone'])
+from django.db import models, migrations
+import django.contrib.gis.db.models.fields
 
 
-    def backwards(self, orm):
-        
-        # Deleting model 'Timezone'
-        db.delete_table('timezones_timezone')
+class Migration(migrations.Migration):
 
+    dependencies = [
+    ]
 
-    models = {
-        'timezones.timezone': {
-            'Meta': {'object_name': 'Timezone'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'mpoly': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {}),
-            'tzid': ('django.db.models.fields.CharField', [], {'max_length': '128'})
-        }
-    }
-
-    complete_apps = ['timezones']
+    operations = [
+        migrations.CreateModel(
+            name='Timezone',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('tzid', models.CharField(max_length=128)),
+                ('mpoly', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
