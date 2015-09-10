ca469e10623647535b78749331a6c5006ef51a1c
Adam Fast <adam@adamfast.com>
2015-09-10 11:44:46 -0500

    Add a management command to cut down the dataset to what the fixture needs, and rebuild the fixture.

---
+++ timezones/management/commands/prep_fixture.py
@@ -0,0 +1,16 @@
+from optparse import make_option
+
+from django.contrib.gis.utils import LayerMapping
+from django.core.management.base import BaseCommand, CommandError
+
+from timezones.models import *
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list
+    args = ''
+    help = 'Delete timezones not needed for tests from the database.'
+
+    def handle(self, *args, **options):
+        unneeded_timezones = Timezone.objects.all().exclude(tzid__in=('America/Chicago', 'America/New_York'))
+        unneeded_timezones.delete()
