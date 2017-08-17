59ca1b5857bfbfea9a2263a50ca0915024f557cf
Adam Fast <adam@adamfast.com>
2017-08-17 11:48:38 -0700

    Update setup.py to modern standards.

--- setup.py
+++ setup.py
@@ -1,26 +1,56 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='geodjango-timezones',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
     version='1.0',
+
     description='Models to store and scripts to load timezone shapefiles to be usable inside a GeoDjango application.',
+    long_description=long_description,  #this is the
+
+    # The project's main homepage.
+    url='https://github.com/adamfast/geodjango-timezones',
+
+    # Author details
     author='Adam Fast',
-    author_email='',
-    url='https://github.com/adamfast/geodjango_timezones',
-    packages=find_packages(),
-    package_data={
-    },
-    include_package_data=True,
-    install_requires=['pytz'],
+    author_email='adamfast@gmail.com',
+
+    # Choose your license
+    license='BSD',
+
+    # See https://PyPI.python.org/PyPI?%3Aaction=list_classifiers
     classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
+
+        # Indicate who your project is intended for
         'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Utilities'
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
     ],
+
+    # What does your project relate to?
+    keywords='timezones gis',
+
+    packages=["timezones"],
+
 )
